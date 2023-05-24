137e4395aa7707416cf3840cb08a0b7aef49a32e
Giovanni Panzetta <giodevcoding@gmail.com>
2023-05-24 11:37:23 -0500

    add location router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/example";
+import { locationRouter } from "./routers/location";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { exampleRouter } from "~/server/api/routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  locations: locationRouter
 });
 
 // export type definition of API
