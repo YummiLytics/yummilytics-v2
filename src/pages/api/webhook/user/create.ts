a673fd3a75a16b65ec7a373840b86c3c9b19c207
Giovanni Panzetta <giodevcoding@gmail.com>
2023-06-15 21:46:28 -0500

    unprotected user create webhook

--- src/pages/api/webhook/user/create.ts
+++ src/pages/api/webhook/user/create.ts
@@ -1,7 +1,19 @@
+import { UserWebhookEvent } from "@clerk/nextjs/dist/server";
 import { NextApiResponse } from "next";
 import { NextRequest } from "next/server";
+import { prisma } from "~/server/db";
 
-export default function handler(req: NextRequest, res: NextApiResponse) {
-  console.log(req);  
-  res.status(200).json({req: req.headers});
+export default async function handler(req: NextRequest, res: NextApiResponse) {
+  // TODO: IMPORTANT!: Add svix verification for webhook request<F2>
+  //@ts-ignore
+  const event = req.body?.evt as UserWebhookEvent;
+  switch (event.type) {
+    case "user.created":
+      await prisma.user.create({
+        data: {
+          clerkId: event.data.id,
+        },
+      });
+      return res.status(200).json({ req: req.headers });
+  }
 }
