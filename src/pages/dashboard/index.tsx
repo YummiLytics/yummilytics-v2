ef563e171597de39a593dbcea8ec4c9ad4a1d5d5
Giovanni Panzetta <giodevcoding@gmail.com>
2023-09-16 13:41:10 -0500

    finish General Info (My Company) Dashboard card

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,52 +1,107 @@
 import type { NextPage } from "next";
 import { Card, CardContent, CardHeader } from "~/components/ui/card";
+import { api } from "~/utils/api";
+import { Skeleton } from "~/components/ui/skeleton";
+import { defaultCompany } from "~/types/defaults";
+import { getPhoneFormatted, getFullAddress } from "~/utils/company-utils";
 
-const GeneralInfo = () => {
+const DashboardCard = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode | React.ReactNode[];
+}) => {
   return (
-    <Card>
-      <CardHeader className="pt-4">
+    <Card className="shadow-md">
+      <CardHeader className="pt-4 pb-3">
         <h2 className="w-56 border-b border-b-slate-200 pb-1 text-lg font-bold">
-          My Company
+          {title}
         </h2>
       </CardHeader>
+      <CardContent>{children}</CardContent>
     </Card>
+  );
+};
+
+const GeneralInfo = () => {
+  const { data, isFetched } = api.companies.getCurrentCompany.useQuery();
+  const company = data ?? {
+    ...defaultCompany,
+    name: "Could not get company name...",
+    repFirstName: "Could not get representative name...",
+    repEmail: "Could not get email address..."
+  }
+
+  return (
+    <DashboardCard title="My Company">
+      <div className="flex items-center gap-2">
+        <span className="font-semibold">Name:</span>
+        <span>
+          {isFetched ? company.name : <Skeleton className="h-5 w-32" />}
+        </span>
+      </div>
+      <div className="flex items-center gap-2">
+        <span className="font-semibold">Address:</span>
+        <span>
+          {isFetched ? (
+            getFullAddress(company)
+          ) : (
+            <Skeleton className="h-5 w-72" />
+          )}
+        </span>
+      </div>
+      <div className="flex items-center gap-2">
+        <span className="font-semibold">Representative Name:</span>
+        <span>
+          {isFetched ? (
+            `${company.repFirstName ?? ""} ${company.repLastName ?? ""}`
+          ) : (
+            <Skeleton className="h-5 w-44" />
+          )}
+        </span>
+      </div>
+      <div className="flex items-center gap-2">
+        <span className="font-semibold">Email:</span>
+        <span>
+          {isFetched ? company.repEmail : <Skeleton className="h-5 w-56" />}
+        </span>
+      </div>
+      <div className="flex items-center gap-2">
+        <span className="font-semibold">Phone:</span>
+        <span>
+          {isFetched ? getPhoneFormatted(company) : <Skeleton className="h-5 w-24" />}
+        </span>
+      </div>
+    </DashboardCard>
   );
 };
 
 const LatestResults = () => {
   return (
-    <Card>
-      <CardHeader className="pt-4">
-        <h2 className="w-56 border-b border-b-slate-200 pb-1 text-lg font-bold">Latest Results</h2>
-      </CardHeader>
-    </Card>
+    <DashboardCard title="Latest Results">
+    </DashboardCard>
   );
 };
 
 const LocationsList = () => {
   return (
-    <Card>
-      <CardHeader className="pt-4">
-        <h2 className="w-56 border-b border-b-slate-200 pb-1 text-lg font-bold">My Locations</h2>
-      </CardHeader>
-    </Card>
+    <DashboardCard title="My Locations">
+    </DashboardCard>
   );
 };
 
 const BenchmarkGroupsList = () => {
   return (
-    <Card>
-      <CardHeader className="pt-4">
-        <h2 className="w-56 border-b border-b-slate-200 pb-1 text-lg font-bold">My Benchmark Groups</h2>
-      </CardHeader>
-    </Card>
+    <DashboardCard title="My Benchmark Groups">
+    </DashboardCard>
   );
 };
 
 const DashboardPage: NextPage = () => {
   return (
     <div className="my-6 lg:container">
-      <Card>
+      <Card className="shadow-md">
         <CardHeader>
           <h1 className="text-2xl font-bold">My YummiLytics Dashboard</h1>
         </CardHeader>
